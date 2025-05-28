2d825c24eaf9c58d232ee3ca403ca66953eb5afa
Mikael Daskalou <mikael.daskalou@hotmail.com>
2025-05-28 13:13:15 +0200

    Add User class and DTOs

---
+++ Application/DTOs/LoginUserResponeDto.cs
@@ -0,0 +1,9 @@
+namespace Application.DTOs
+{
+    public class LoginUserResponeDto
+    {
+        public string Token { get; set; } 
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
