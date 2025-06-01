86faba8b11671216b9ca0257e6a8945fedf2ec65
OscarSystemDeveloper <oscj87@gmail.com>
2025-06-01 15:46:29 +0200

    Add TransactionController with MediatR, Commands, Queries and DTOs

---
+++ Application/Features/TransactionFeatures/Handlers/CreateTransactionCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Features.TransactionFeatures.Commands;
+using Application.Interfaces;
+using Application.Common;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.TransactionFeatures.Handlers
+{
+    public class CreateTransactionCommandHandler : IRequestHandler<CreateTransactionCommand, OperationResult<Transaction>>
+    {
+        private readonly ITransactionService _transactionService;
+
+        public CreateTransactionCommandHandler(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        public async Task<OperationResult<Transaction>> Handle(CreateTransactionCommand request, CancellationToken cancellationToken)
+        {
+            var transaction = new Transaction
+            {
+                UserId = request.TransactionDto.UserId,
+                Amount = request.TransactionDto.Amount,
+                Description = request.TransactionDto.Description,
+                Date = request.TransactionDto.Date,
+                IsIncome = request.TransactionDto.IsIncome
+            };
+
+            var created = await _transactionService.CreateTransactionAsync(transaction);
+
+            return OperationResult<Transaction>.Success(created);
+        }
+    }
+}
---
+++ Application/Features/TransactionFeatures/Queries/GetAllTransactionsQuery.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.TransactionFeatures.Queries
+{
+    public class GetAllTransactionsQuery : IRequest<OperationResult<List<Transaction>>>
+    {
+        public Guid UserId { get; set; }
+
+        public GetAllTransactionsQuery(Guid userId)
+        {
+            UserId = userId;
+        }
+    }
+}
