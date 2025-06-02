8a654f9e82d27a18555aa86aabdeb207b37512c2
OscarSystemDeveloper <oscj87@gmail.com>
2025-06-02 10:23:43 +0200

    Feature: Transaction Advanced Features (Update, Delete, Statistics, Tests)

---
+++ Application/Features/TransactionFeatures/Handlers/GetTransactionsByMonthHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Features.TransactionFeatures.Queries;
+using Application.Interfaces;
+using Application.Common;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.TransactionFeatures.Handlers
+{
+    public class GetTransactionsByMonthHandler : IRequestHandler<GetTransactionsByMonthQuery, OperationResult<List<Transaction>>>
+    {
+        private readonly ITransactionService _transactionService;
+
+        public GetTransactionsByMonthHandler(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        public async Task<OperationResult<List<Transaction>>> Handle(GetTransactionsByMonthQuery request, CancellationToken cancellationToken)
+        {
+            var transactions = await _transactionService.GetAllTransactionsAsync(request.UserId);
+
+            var filtered = transactions
+                .Where(t => t.Date.Year == request.Year && t.Date.Month == request.Month)
+                .ToList();
+
+            return OperationResult<List<Transaction>>.Success(filtered);
+        }
+    }
+}
---
+++ Application/Validators/UpdateTransactionDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTOs;
+using FluentValidation;
+
+namespace Application.Validators
+{
+    public class UpdateTransactionDtoValidator : AbstractValidator<UpdateTransactionDto>
+    {
+        public UpdateTransactionDtoValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.UserId).NotEmpty();
+            RuleFor(x => x.Amount).GreaterThan(0);
+            RuleFor(x => x.Description).NotEmpty().MaximumLength(200);
+            RuleFor(x => x.Date).NotEmpty();
+        }
+    }
+}
